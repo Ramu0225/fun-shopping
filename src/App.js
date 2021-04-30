eb6da87698c0cde1d920040d0397a3a77ba5e341
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-04-30 15:26:45 +0200

    added shoppage, contact, logo to the header tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './component/header/header.component.jsx'; 
 
  function App() {
   return (
       <div>
-        <HomePage />
+        <Header />
+        <Switch>
+          <Route exact path = '/' component={HomePage}/>
+          <Route path= '/shop' component={ShopPage}/>
+        </Switch>
+        
       </div>
   );
 }
