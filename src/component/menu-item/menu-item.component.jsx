eb71e1efe586d789a679e639472bb4bf183e3ac0
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-05-18 12:27:14 +0200

    updated with font size with open sans condensed

--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -8,7 +8,7 @@ const MenuItem = ({title, imageUrl, size}) =>(
                 style={{
                 backgroundImage: `url(${imageUrl})`}} />
         <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='title'><strong>{title.toUpperCase()}</strong></span>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
