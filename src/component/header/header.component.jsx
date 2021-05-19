42e6b015df7382b7d07a9b8c517d9ba82d39d99d
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-05-19 12:42:16 +0200

    updated with cart menu and dropdown menu

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import{ReactComponent as Logo} from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils';
-
+import CartIcon from '../cart-icon/cart-icon.components';
 import './header.styles.scss';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 
 const Header = ({ currentUser }) =>(
     <div className= 'header'>
@@ -22,8 +24,9 @@ const Header = ({ currentUser }) =>(
                 <div className='option' onClick={() => auth.signOut()}>SIGNOUT</div>
                 : <Link className='option' to='/signin'>SIGNIN</Link>
             }
+            <CartIcon />
         </div>
-    
+            <CartDropdown />
     </div>
 
 );
