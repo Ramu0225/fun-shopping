62097f91b1c2d8d88ffc7ea4c1741190f48190e7
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-05-19 13:28:06 +0200

    updated with carticon on click hide option

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -8,7 +8,7 @@ import './header.styles.scss';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 
-const Header = ({ currentUser }) =>(
+const Header = ({ currentUser, hidden }) =>(
     <div className= 'header'>
         <Link className= 'logo-container' to="/">
             <Logo className= 'logo'/>
@@ -26,12 +26,14 @@ const Header = ({ currentUser }) =>(
             }
             <CartIcon />
         </div>
-            <CartDropdown />
+        {hidden? null: <CartDropdown />}
     </div>
 
 );
 
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser}, cart: {hidden}}) =>({
+    currentUser,
+    hidden
+
 })
 export default connect(mapStateToProps)(Header);
