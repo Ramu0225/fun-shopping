a6599bfcdea3ce982f77c34f525fa40421215ac9
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-05-19 20:22:40 +0200

    updated with cart item

---
+++ src/component/cart-item/cart-item.components.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './cart-item.scss';
+
+const CartItem = ({ item: {imageUrl, price, name}})=>(
+
+    <div className='cart-item'>
+        <img src={imageUrl} alt='item' />
+        <div className='item-details'>
+            <span className='name'>{name}</span>
+            <span className='price'>${price}</span>
+        </div>    
+    </div>
+
+);
+
+export default CartItem;
