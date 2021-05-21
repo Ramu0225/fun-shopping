4f3f56e6d0b1589a4c6db7fc1ef92c320db82bab
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-05-21 11:55:44 +0200

    created cart-item men and connected with cartdropdown menu with redux reducer

--- src/component/cart-item/cart-item.components.jsx
+++ src/component/cart-item/cart-item.components.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './cart-item.scss';
 
-const CartItem = ({ item: {imageUrl, price, name}})=>(
+const CartItem = ({ item: {imageUrl, price, name, quantity}})=>(
 
     <div className='cart-item'>
         <img src={imageUrl} alt='item' />
         <div className='item-details'>
             <span className='name'>{name}</span>
-            <span className='price'>${price}</span>
+            <span className='price'>{quantity} x ${price}</span>
         </div>    
     </div>
 
